6b9473ca44e1c9648b7bf0852c5ec76e0a4faf94
itsdddaniel <daniel.arteaga@unah.hn>
2021-04-18 09:20:31 -0600

    Se modificaron las inserciones

--- Script de Base de Datos/DMS.sql
+++ Script de Base de Datos/DMS.sql
@@ -50,11 +50,36 @@ INSERT INTO Categoria(nombre) VALUES
 
 INSERT INTO Categoria_Producto(id_producto,id_categoria) VALUES
 
-    (1),
-    (2),
-    (3),
-    (4),
-    (5),
+    (3010,1),
+    (3020,1),
+    (3110,1),
+    (3120,1),
+    (3210,1),
+
+    (3310,2),
+    (3320,2),
+    (3330,2),
+    (3340,2),
+    (3350,2),
+
+    (3410,3),
+    (3420,3),
+    (3510,3),
+    (3520,3),
+    (3610,3),
+
+    (3710,4),
+    (3810,4),
+    (3820,4),
+    (3910,4),
+    (3920,4),
+
+    (4010,5),
+    (4020,5),
+    (4110,5),
+    (4120,5),
+    (4130,5),
+
     (2010,6),
     (2011,6),
     (2012,6),
@@ -66,6 +91,7 @@ INSERT INTO Categoria_Producto(id_producto,id_categoria) VALUES
     (2031,6),
     (2032,6),
     (2033,6),
+
     (1610,7),
     (1611,7),
     (1620,7),
@@ -73,12 +99,14 @@ INSERT INTO Categoria_Producto(id_producto,id_categoria) VALUES
     (1630,7),
     (1631,7),
     (1632,7),
+
     (1710,8),
     (1711,8),
     (1720,8),
     (1721,8),
     (1730,8),
     (1731,8),
+
     (1910,9),
     (1920,9),
     (1921,9),
@@ -132,38 +160,37 @@ INSERT INTO Estado(tipoEstado) VALUES
 
 INSERT INTO Producto(id_producto,id_marca,id_estado,id_novedad,nombre,PrecioCompra,PrecioVenta,fechaEntrada,urlProducto,Descripcion) VALUES
 
-    (1,1,1,1,"iPhone 6",500,1000.3,NOW(),"https://drive.google.com/uc?id=17vHt3qBz70TRabwCnmr6ya553CLRc2TL","Smartphone Apple"),
-    (1,2,1,2,"Samsung Galaxy S10",765,7094.56,NOW(),"https://drive.google.com/uc?id=1nF7HA7o5bNjyK56FritGXr5EEpQPW4i-","Smartphone Samsung"),
-    (1,3,1,3,"iPhone 8",800,4000.78,NOW(),"https://drive.google.com/uc?id=1051KQ0LwUfN77in3AsYSwSlI_PH0FbJU","Smartphone Apple"),
-    (1,4,1,4,"Huawei Mate Pro",234,3045.45,NOW(),"https://drive.google.com/uc?id=1M9CO38K6pZOrFcB65LSsif8HWGG-USRf","Smartphone Huawei"),
-    (1,5,1,5,"Samsung Galaxy Note S10",467,8500.58,NOW(),"https://drive.google.com/uc?id=1ykyrqAAw_nFxJ1iksXbQuvXnOWPOLTVM","Smartphone Samsung"),
+    (3010,1,1,1,"iPhone 6",500,1000.3,NOW(),"https://drive.google.com/uc?id=17vHt3qBz70TRabwCnmr6ya553CLRc2TL","Smartphone Apple"),
+    (3020,1,1,1,"iPhone 8",800,4000.78,NOW(),"https://drive.google.com/uc?id=1051KQ0LwUfN77in3AsYSwSlI_PH0FbJU","Smartphone Apple"),
+    (3110,1,1,1,"Samsung Galaxy S10",765,7094.56,NOW(),"https://drive.google.com/uc?id=1nF7HA7o5bNjyK56FritGXr5EEpQPW4i-","Smartphone Samsung"),
+    (3120,1,1,1,"Samsung Galaxy Note S10",467,8500.58,NOW(),"https://drive.google.com/uc?id=1ykyrqAAw_nFxJ1iksXbQuvXnOWPOLTVM","Smartphone Samsung"),
+    (3210,1,1,1,"Huawei Mate Pro",234,3045.45,NOW(),"https://drive.google.com/uc?id=1M9CO38K6pZOrFcB65LSsif8HWGG-USRf","Smartphone Huawei"),
 
-    (2,6,1,6,"Dell XPS 10",596,5000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
-    (2,7,1,7,"Dell XPS 11",865,6000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
-    (2,8,1,8,"Dell XPS 12",934,7000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
-    (2,9,1,9,"Dell XPS 13",1000,8000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
-    (2,10,1,10,"Dell XPS 14",1500,9000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
+    (3310,1,1,1,"Dell XPS 10",596,5000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
+    (3320,1,1,1,"Dell XPS 11",865,6000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
+    (3330,1,1,1,"Dell XPS 12",934,7000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
+    (3340,1,1,1,"Dell XPS 13",1000,8000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
+    (3350,1,1,1,"Dell XPS 14",1500,9000.12,NOW(),"https://drive.google.com/uc?id=1RIgap62OsWQayYR5h7Z0XS4F_VGgFNax","Desktop Dell"),
 
-    (3,11,1,11,"Nintendo Switch",456,5000,NOW(),"https://drive.google.com/uc?id=1Z2OdBBFzAuGMV5noqX0gKPsJkXTOWzmu","Consola Nintendo Switch"),
-    (3,12,1,12,"Playstation 5",345,10000,NOW(),"https://drive.google.com/uc?id=1UvXBWO5IgQoKArQofcPKYGKL8CHKgVAG","Consola Playstation 5"),
-    (3,13,1,13,"XBOX One",678,4000,NOW(),"https://drive.google.com/uc?id=1owp7zHLpa95cn6GmJkvx41sw04uZ0qZE","Consola XBOX One"),
-    (3,14,1,14,"Nintendo Wii U",290,2000,NOW(),"https://drive.google.com/uc?id=1C6nzi09BKbBtntbCzFrQbh-Qwrc2vmk2","Consola Nintendo Wii U"),
-    (3,15,1,15,"Playstation 4",500,3000,NOW(),"https://drive.google.com/uc?id=1QgCpMe-YmUvFSSqSvwIyHfmAYPAwhkPU","Consola Playstation 4"),
+    (3410,1,1,1,"Nintendo Switch",456,5000,NOW(),"https://drive.google.com/uc?id=1Z2OdBBFzAuGMV5noqX0gKPsJkXTOWzmu","Consola Nintendo Switch"),
+    (3420,1,1,1,"Nintendo Wii U",290,2000,NOW(),"https://drive.google.com/uc?id=1C6nzi09BKbBtntbCzFrQbh-Qwrc2vmk2","Consola Nintendo Wii U"),
+    (3510,1,1,1,"Playstation 4",500,3000,NOW(),"https://drive.google.com/uc?id=1QgCpMe-YmUvFSSqSvwIyHfmAYPAwhkPU","Consola Playstation 4"),
+    (3520,1,1,1,"Playstation 5",345,10000,NOW(),"https://drive.google.com/uc?id=1UvXBWO5IgQoKArQofcPKYGKL8CHKgVAG","Consola Playstation 5"),
+    (3610,1,1,1,"XBOX One",678,4000,NOW(),"https://drive.google.com/uc?id=1owp7zHLpa95cn6GmJkvx41sw04uZ0qZE","Consola XBOX One"),
 
+    (3710,1,1,1,"Apple MacBook Air MQD32HN/A",345,8234.12,NOW(),"https://drive.google.com/uc?id=141vYJR_rQMEmEGGEGgRP3OwnEHq5DB3u","Laptop Macbook"),
+    (3810,1,1,1,"Acer Aspire 7 A715-75G",457,6500.43,NOW(),"https://drive.google.com/uc?id=1EafQ22qYj6wkNLs0PbFhk0UdmCxAlx0H","Laptop Acer"),
+    (3820,1,1,1,"Acer One 14 Z2-485",731,2000.00,NOW(),"https://drive.google.com/uc?id=1R6xWLTAwuoqJoHpxFzFr8UgxyRq0H40R","Laptop Acer"),
+    (3910,1,1,1,"HP 15q-dy0007au",567,5900.23,NOW(),"https://drive.google.com/uc?id=1d9_PDeJQOm8LvA6nX1fw6daYttT-Vll8","Laptop HP"),
+    (3920,1,1,1,"HP Pavilion Gaming 15-ec0101AX",689,4500.85,NOW(),"https://drive.google.com/uc?id=1qf4gFi9KE6-xjJwA4rW7oAKf1SxX_plx","Laptop HP"),
 
-    (4,16,1,16,"Apple MacBook Air MQD32HN/A",345,8234.12,NOW(),"https://drive.google.com/uc?id=141vYJR_rQMEmEGGEGgRP3OwnEHq5DB3u","Laptop Macbook"),
-    (4,17,1,17,"Acer Aspire 7 A715-75G",457,6500.43,NOW(),"https://drive.google.com/uc?id=1EafQ22qYj6wkNLs0PbFhk0UdmCxAlx0H","Laptop Acer"),
-    (4,18,1,18,"HP 15q-dy0007au",567,5900.23,NOW(),"https://drive.google.com/uc?id=1d9_PDeJQOm8LvA6nX1fw6daYttT-Vll8","Laptop HP"),
-    (4,19,1,19,"HP Pavilion Gaming 15-ec0101AX",689,4500.85,NOW(),"https://drive.google.com/uc?id=1qf4gFi9KE6-xjJwA4rW7oAKf1SxX_plx","Laptop HP"),
-    (4,20,2,20,"Acer One 14 Z2-485",731,2000.00,NOW(),"https://drive.google.com/uc?id=1R6xWLTAwuoqJoHpxFzFr8UgxyRq0H40R","Laptop Acer"),
+    (4010,1,1,1,"Canon G4111 Wi-Fi",435,9000,NOW(),"https://drive.google.com/uc?id=11-S3tdfzH91ji3cSYtnYDuUKWmZmTxy7","Impresora Canon"),
+    (4020,1,1,1,"Canon Pixma E402 USB",200,2000,NOW(),"https://drive.google.com/uc?id=1lgLOFEw7oPBO74FrOV_Lt0D3gULfc6z1","Impresora Canon"),
+    (4110,1,1,1,"HP Smart Tank 500 USB",300,6000,NOW(),"https://drive.google.com/uc?id=1WiVwqDxHSdVtiKITTY6vfEhNtxCtyiPJ","Impresora HP"),
+    (4120,1,1,1,"HP Smart Tank 530 USB",400,9000,NOW(),"https://drive.google.com/uc?id=1FZ67fFTJhEMb5fSnFATUCObPR4qmJrnT","Impresora HP"),
+    (4130,1,1,1,"HP Smart Tank 515 Wi-Fi",500,7000,NOW(),"https://drive.google.com/uc?id=1XfqIBVdmmzPujxRhFwl2Xm_fYQpQP059","Impresora HP"),
 
-    (5,21,1,21,"Canon G4111 Wi-Fi",435,9000,NOW(),"https://drive.google.com/uc?id=11-S3tdfzH91ji3cSYtnYDuUKWmZmTxy7","Impresora Canon"),
-    (5,22,1,22,"Canon Pixma E402 USB",200,2000,NOW(),"https://drive.google.com/uc?id=1lgLOFEw7oPBO74FrOV_Lt0D3gULfc6z1","Impresora Canon"),
-    (5,23,1,23,"HP Smart Tank 500 USB",300,6000,NOW(),"https://drive.google.com/uc?id=1WiVwqDxHSdVtiKITTY6vfEhNtxCtyiPJ","Impresora HP"),
-    (5,24,1,24,"HP Smart Tank 530 USB",400,9000,NOW(),"https://drive.google.com/uc?id=1FZ67fFTJhEMb5fSnFATUCObPR4qmJrnT","Impresora HP"),
-    (5,25,1,25,"HP Smart Tank 515 Wi-Fi",500,7000,NOW(),"https://drive.google.com/uc?id=1XfqIBVdmmzPujxRhFwl2Xm_fYQpQP059","Impresora HP")
-
-    (2010,1,1,1,"Samsung Smart TV",10000,15997.00,NOW(),"",'Televisor Smart TV de 58" 4K UHD'),
+    (2010,1,1,1,"Samsung Smart TV",10000,15997.00,NOW(),"","Televisor Smart TV de 58'' 4K UHD"),
     (2011,1,1,1,"Samsung Smart TV",7000,11000.00,NOW(),"",'Televisor Smart TV de 43" 4K UHD'),
     (2012,1,1,1,"Samsung Crystal TV",9000,13000.00,NOW(),"",'Televisor de 50" 4K UHD'),
     (2013,1,1,1,"Samsung TV",5000,8000.00,NOW(),"",'Televisor  de 32" HD'),
@@ -197,3 +224,5 @@ INSERT INTO Producto(id_producto,id_marca,id_estado,id_novedad,nombre,PrecioComp
     (1931,1,1,1,"360 HDMI",100,200.00,NOW(),"",'Cable tipo HDMI'),
     (1932,1,1,1,"Headset Recon 70X",900,1200.00,NOW(),"",'Cascos turtle Beach')
 ;
+
+SELECT * FROM Categoria_Producto JOIN Producto ON Producto.id_producto = Categoria_Producto.id_producto WHERE id_categoria = 1;
