c2de7ca53346a1fbd67de9d73be1a89e23a29e3b
itsdddaniel <daniel.arteaga@unah.hn>
2021-04-19 20:13:25 -0600

    Se añadio ventanas modales para login y carrito.

--- Codigo/scripts/script.js
+++ Codigo/scripts/script.js
@@ -1,3 +1,47 @@
+var modalLogin = document.getElementById("modalLogin");
+var btnLogin = document.getElementById("login");
+var spanLogin = document.getElementsByClassName("closeLogin")[0];
+
+btnLogin.onclick = function() 
+{
+  modalLogin.style.display = "block";
+}
+
+spanLogin.onclick = function() 
+{
+  modalLogin.style.display = "none";
+}
+
+window.onclick = function(event) 
+{
+  if (event.target == modalLogin) 
+  {
+    modalLogin.style.display = "none";
+  }
+}
+
+var modal = document.getElementById("modalCart");
+var btn = document.getElementById("cart");
+var span = document.getElementsByClassName("closeCart")[0];
+
+btn.onclick = function() 
+{
+  modal.style.display = "block";
+}
+
+span.onclick = function() 
+{
+  modal.style.display = "none";
+}
+
+window.onclick = function(event)
+ {
+  if (event.target == modal) 
+  {
+    modal.style.display = "none";
+  }
+}
+
 var slideIndex = 0;
 showSlides();
 
