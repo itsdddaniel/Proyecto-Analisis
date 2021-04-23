76f5a1f5355b74bbc3fa091f0d35f01faf9416f7
itsdddaniel <xdarktheboss@gmail.com>
2021-04-23 14:04:52 -0600

    Se removieron componentes.

--- Codigo/scripts/script.js
+++ Codigo/scripts/script.js
@@ -51,28 +51,6 @@ window.onload = function()
     }
 }
 
-var modalLogin = document.getElementById("modalLogin");
-var btnLogin = document.getElementById("login");
-var spanLogin = document.getElementsByClassName("closeLogin")[0];
-
-btnLogin.onclick = function() 
-{
-  modalLogin.style.display = "block";
-}
-
-spanLogin.onclick = function() 
-{
-  modalLogin.style.display = "none";
-}
-
-window.onclick = function(event) 
-{
-  if (event.target == modalLogin) 
-  {
-    modalLogin.style.display = "none";
-  }
-}
-
 var modal = document.getElementById("modalCart");
 var btn = document.getElementById("cart");
 var span = document.getElementsByClassName("closeCart")[0];
--- Codigo/views/global.js
+++ Codigo/views/global.js
@@ -1,25 +1,3 @@
-var modalLogin = document.getElementById("modalLogin");
-var btnLogin = document.getElementById("login");
-var spanLogin = document.getElementsByClassName("closeLogin")[0];
-
-btnLogin.onclick = function() 
-{
-  modalLogin.style.display = "block";
-}
-
-spanLogin.onclick = function() 
-{
-  modalLogin.style.display = "none";
-}
-
-window.onclick = function(event) 
-{
-  if (event.target == modalLogin) 
-  {
-    modalLogin.style.display = "none";
-  }
-}
-
 var modal = document.getElementById("modalCart");
 var btn = document.getElementById("cart");
 var span = document.getElementsByClassName("closeCart")[0];
@@ -35,7 +13,7 @@ span.onclick = function()
 }
 
 window.onclick = function(event)
- {
+{
   if (event.target == modal) 
   {
     modal.style.display = "none";
